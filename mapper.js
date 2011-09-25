9aafa6577d7974caf2eb59ddf1cfba03d32638e5
Michail Pishchagin <mblsha@users.sourceforge.net>
2011-09-25 17:57:05 +0400

    Added route selector.

--- mapper.js
+++ mapper.js
@@ -1,5 +1,7 @@
 var Mapper = Object.extend({
 	constructor: function() {
+		this.mapListForm = document.getElementById("MapListForm");
+
 		this.map = new YMaps.Map(document.getElementById("YMapsID"));
 		this.map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
 
@@ -14,11 +16,13 @@ var Mapper = Object.extend({
 		[
 			{ name: "Москва 2010",
 			  style: "default#pinkPoint",
-			  fileName: "msk2010-all.json"
+			  fileName: "msk2010-all.json",
+			  center: new YMaps.GeoPoint(37.64, 55.76)
 			},
 			{ name: "Москва 2011",
 			  style: "default#darkbluePoint",
-			  fileName: "msk2011-all.json"
+			  fileName: "msk2011-all.json",
+			  center: new YMaps.GeoPoint(37.64, 55.76)
 			}
 		].forEach((function(data) {
 			this.loadData(data);
@@ -34,7 +38,7 @@ var Mapper = Object.extend({
 				new YMaps.GeoPoint(kp.lon, kp.lat),
 				{ style: this.data[name].style }
 			);
-			kp.placemark.setIconContent(kp.id);
+			// kp.placemark.setIconContent(kp.id);
 			kp.placemark.name = kp.name + " (" + name + ")";
 			kp.placemark.description = kp.description;
 			this.map.addOverlay(kp.placemark);
@@ -47,6 +51,38 @@ var Mapper = Object.extend({
 		}
 	},
 
+	getPlacemark: function(kp, name) {
+		if (kp.placemark)
+			return kp.placemark;
+
+		if (!kp.lat || !kp.lon)
+			return null;
+
+		kp.placemark = new YMaps.Placemark(
+			new YMaps.GeoPoint(kp.lon, kp.lat),
+			{ style: this.data[name].style }
+		);
+		// kp.placemark.setIconContent(kp.id);
+		kp.placemark.name = kp.name + " (" + name + ")";
+		kp.placemark.description = kp.description;
+		return kp.placemark;
+	},
+
+	setKpVisible: function(name, visible) {
+		var d = this.data[name];
+
+		for (var i in d.kpList) {
+			var placemark = this.getPlacemark(d.kpList[i], name);
+			if (placemark == null)
+				continue;
+
+			if (visible)
+				this.map.addOverlay(placemark);
+			else
+				this.map.removeOverlay(placemark);
+		}
+	},
+
 	loadData: function(data) {
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", "data/" + data.fileName, true);
@@ -55,8 +91,28 @@ var Mapper = Object.extend({
 				return;
 
 			this.data[data.name] = data;
-			this.data[data.name].kpList = JSON.parse(xhr.responseText);
-			this.processData(data.name);
+			var d = this.data[data.name];
+			d.kpList = JSON.parse(xhr.responseText);
+
+			var li = document.createElement('li');
+			var label = document.createElement('label');
+			label.textContent = data.name;
+
+			label.onmouseover = function() { this.className += " hover"; };
+			label.onmouseout = function() { this.className = this.className.replace(" hover", ""); };
+
+			d.mapCheckbox = document.createElement('input');
+			d.mapCheckbox.setAttribute('type', 'checkbox');
+			d.mapCheckbox.setAttribute('id',   data.fileName);
+
+			d.mapCheckbox.onclick = (function(id, name) {
+				var checkbox = document.getElementById(id);
+				this.setKpVisible(name, checkbox.checked);
+			}).bind(this, data.fileName, data.name);
+
+			li.appendChild(label);
+			label.prependChild(d.mapCheckbox);
+			this.mapListForm.appendChild(li);
 		}).bind(this, data)
 		xhr.send();
 	}
